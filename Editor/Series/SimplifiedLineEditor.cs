7efe6f1eea64f23fa023469e2c7b86d73e49b9b4
Kyle Li <kyleseanli@gmail.com>
2025-07-16 08:37:45 -0400

    Update SimplifiedLineEditor.cs

--- Editor/Series/SimplifiedLineEditor.cs
+++ Editor/Series/SimplifiedLineEditor.cs
@@ -11,6 +11,13 @@ namespace XCharts.Editor
             PropertyField("m_YAxisIndex");
             PropertyField("m_LineType");
             //PropertyField("m_Clip");
+            var m_TooltipCutoffEnable = baseProperty.FindPropertyRelative("m_TooltipCutoffEnable");
+            PropertyField(m_TooltipCutoffEnable);
+            if (m_TooltipCutoffEnable.boolValue)
+            {
+                PropertyField("m_TooltipCutoffComparison");
+                PropertyField("m_TooltipCutoffValue");
+            }
             PropertyField("m_LineStyle");
             PropertyField("m_ItemStyle");
             PropertyField("m_Animation");
